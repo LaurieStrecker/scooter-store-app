76487086a006f63dd6676b278bd7984fe2c5016d
LahRee <Laurie.Strecker@gmail.com>
2020-08-03 14:45:37 -0700

    added header, README.md, removed package.lock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+//import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+       <header className="App-header">
+            <h1>SCOOTER STORE</h1>
+            <Button>Get Started</Button>
       </header>
+
+        <Container>
+          <Row>
+            <Col> 
+           </Col>
+          </Row>
+        </Container>
+     
     </div>
   );
 }
